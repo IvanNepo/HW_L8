98f413e498ecf7bd493b4dfdee3c874f93366341
Ivan <156950275+IvanNepo@users.noreply.github.com>
2024-02-28 19:36:18 +0300

    Add files via upload

---
+++ HW_L8/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+// Интерфейс с методом Eat
+interface IEatable
+{
+    void Eat();
+}
+
+// Абстрактный класс Animal с свойством Name
+abstract class Animal
+{
+    public string Name { get; private set; }
+
+    public void SetName(string name)
+    {
+        Name = name;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+}
+
+// Класс Dog, реализующий интерфейс IEatable
+class Dog : Animal, IEatable
+{
+    public void Eat()
+    {
+        Console.WriteLine($"{Name} ест корм.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите имя собаки: ");
+        string dogName = Console.ReadLine();
+
+        Dog dog = new Dog();
+        dog.SetName(dogName);
+
+        Console.WriteLine($"Имя собаки: {dog.GetName()}");
+        ((IEatable)dog).Eat(); // Вызов метода Eat через интерфейс
+    }
+}
---
+++ HW_L8/HW_L8/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace HW_L8
+{
+    internal class Program;
+
+// Абстрактный класс Animal
+abstract class Animal
+    {
+        public string Name { get; private set; }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public abstract void Eat();
+    }
+
+    // Класс Dog, наследующий абстрактный класс Animal
+    class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine($"{Name} ест корм.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя собаки: ");
+            string dogName = Console.ReadLine();
+
+            Dog dog = new Dog();
+            dog.SetName(dogName);
+
+            Console.WriteLine($"Имя собаки: {dog.GetName()}");
+            dog.Eat();
+        }
+    }
+}
